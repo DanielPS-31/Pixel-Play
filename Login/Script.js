e9de8ad4eefc7e3b822f745c2e229c7ac72d1a91
Daniel de Paiva <d4nielps105@gmail.com>
2025-12-02 15:05:43 -0300

    commit inicial

---
+++ Login/Script.js
@@ -0,0 +1,86 @@
+function Join() {
+
+  const email2 = document.getElementById("Email2").value;
+  const password2 = document.getElementById("Password2").value;
+  
+  if (email2 == "") {
+    return alert("Email vazio!");
+  }
+  if (password2 == "") {
+    return alert("Senha vazia!");
+  }
+
+    for (let i = 0; i < localStorage.length; i++) {
+
+    let id = localStorage.key(i);
+    let dados = localStorage.getItem(id);
+    var users = JSON.parse(dados);
+
+    if(password2==users.Password_ && email2==users.Email_){
+        var chave = id;
+        var status = "Ativo";
+        var usuario ={
+            Username_ : users.Username_,
+            Email_ : email2,
+            Password_ : password2,
+            Status_ : status
+                     };
+    
+        localStorage.setItem( chave, JSON.stringify(usuario));
+        window.location.href = "/HomeScreenLogged/HomeScreenLogged.html";
+        return false;
+    }else if(i>=localStorage.length){
+        alert("Não tem cadastro");        
+    }
+}
+}
+
+function Criar_conta() {
+  var chave = localStorage.length;
+    chave++;
+  const status = "Desativado";
+  const username = document.getElementById("Username").value;
+  const email = document.getElementById("Email").value;
+  const password = document.getElementById("Password").value;
+  const password2 = document.getElementById("Password3").value;
+
+  if (username == "") {
+    return alert("Nome de usuário vazio!");
+  }
+  if (email == "") {
+    return alert("Email vazio!");
+  }
+  if (password == "") {
+    return alert("Senha vazia!");
+  }
+  if (password == password2) {
+
+    var usuario ={
+        Username_ : username,
+        Email_ : email,
+        Password_ : password,
+        Status_ : status
+      };
+    
+      localStorage.setItem( chave, JSON.stringify(usuario));
+
+    window.location.href = "/Login/Login.html";
+    return;
+  } else {
+    alert("Confirme sua senha!");
+    return;
+  }
+}
+
+function ShowPassword() {
+  var passwords = document.getElementsByClassName("Password");
+
+  for (let i = 0; i < passwords.length; i++) {
+    if (passwords[i].type === "password") {
+      passwords[i].type = "text";
+    } else {
+      passwords[i].type = "password";
+    }
+  }
+}
+
---
+++ script.js
@@ -0,0 +1,97 @@
+let Usuario = " ";
+
+function Entrar() {
+
+    window.location.href = "Login/Login.html";
+
+}
+
+function Criar() {
+    
+    window.location.href = "Login/Create.html";
+
+}
+
+function Join_Screen() {
+
+    window.location.href = "/Join/Join.html";
+
+}
+
+function History_Screen() {
+    
+    window.location.href = "/History/History.html";
+
+}
+
+function Logout(){
+    
+    
+    for (let i = 0; i <= localStorage.length; i++) {
+    let id = localStorage.key(i);
+    let dados = localStorage.getItem(id);
+    var users = JSON.parse(dados);
+    if(users.Status_=="Ativo"){
+        var chave = id;
+        var email = users.Email_;
+        var password = users.Password_;
+        var status = "Desativado";
+        var usuario ={
+            Username_ : users.Username_,
+            Email_ : email,
+            Password_ : password,
+            Status_ : status
+                     };
+    
+        localStorage.setItem( chave, JSON.stringify(usuario));
+        window.location.href = "/Login/Login.html";
+        return false;
+    }
+}
+
+}
+
+window.onload = function(){
+
+
+    
+
+    let j=0;
+
+    for (j = 0; j <= localStorage.length; j++) {
+
+        let id = localStorage.key(j);
+        let dados = localStorage.getItem(id);
+        var users = JSON.parse(dados);
+        
+        if (users) {
+
+            if(users.Status_ == "Ativo"){
+            Usuario = users.Username_
+            const Welcome = document.getElementById("Welcome-User");
+            
+            if (Welcome) {
+                Welcome.innerHTML = Usuario + ", Bem vindo ao Menu!";  
+            }
+
+
+            return false;
+        }
+
+        }
+
+    
+    }
+
+        if(j>this.localStorage.length){
+
+            if (window.location.pathname != "Index.html") {
+                return;
+            }
+
+            window.location.href = "/Login/Login.html";
+        }
+
+}
+
+
